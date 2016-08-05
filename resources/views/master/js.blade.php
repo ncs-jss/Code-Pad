3b3a013cefd5ff028b480d120369f09c74fcea65
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-08-05 20:50:00 +0530

    Demo Blade Created

---
+++ resources/views/master/js.blade.php
@@ -0,0 +1,18 @@
+<!-- #### JAVASCRIPT FILES ### -->
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')
+    </script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src = "{{ URL::asset('public/assets/js/jquery.cookie.js')}}"></script>
+    <script src = "{{ URL::asset('public/assets/js/waypoints.min.js')}}"></script>
+    <script src = "{{ URL::asset('public/assets/js/jquery.counterup.min.js')}}"></script>
+    <script src = "{{ URL::asset('public/assets/js/jquery.parallax-1.1.3.js')}}"></script>
+    <script src = "{{ URL::asset('public/assets/js/front.js')}}"></script>
+    <script src = "{{ URL::asset('public/assets/js/login.js')}}"></script>
+
+
+    <!-- owl carousel -->
+    <script src = "{{ URL::asset('public/assets/js/owl.carousel.min.js')}}"></script>
+
