f9195b9c55f12ebbb8acb15495aecc54fcfb9855
ankitjain28may <ankitjain28may77@gmail.com>
2016-08-11 17:47:14 +0530

    Remove bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,7 +117,7 @@ elseif (Session::get('type')=='teacher') {
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{!! asset('node_modules/place-holder.js/place-holder.min.js') !!}" ></script>
+    <script type="text/javascript" src="{!! asset('public/node_modules/place-holder.js/place-holder.min.js') !!}" ></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 
     @yield('script')
