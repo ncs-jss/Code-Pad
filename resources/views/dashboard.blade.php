5ba2fd5b9e793e349e369857a5ce1e9de42c5963
Anant Jain <anantjain60@gmail.com>
2018-06-25 21:41:12 +0530

    Front Page
    Patch #19

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 if(Auth::guard('admin')->check()):
     $result = Auth::guard('admin')->user();
 elseif(Auth::guard('student')->check()):
@@ -7,7 +7,7 @@ else:
     $result = Auth::guard('teacher')->user();
 endif;
 // echo $message;
-?>
+@endphp
 @extends('layouts.layout')
   @section('body')
     <div class="custom-flash {{ Session::get('class') }} ">{{ Session::get('message') }}</div>
@@ -42,48 +42,6 @@ endif;
                 </div>
               </div>
             </div>
-            <div class="item">
-              <div class="row">
-                <div class="col-sm-7">
-                  <img class="img-responsive" src="{{URL::asset('public/assets/img/template-easy-code.png')}}" alt="">
-                </div>
-                <div class="col-sm-5">
-                  <center>
-                    <h1>
-                      CODE <br>
-                      COMPETE <br>
-                      COLLABORATE
-                    </h1>
-                    <ul class="list-style-none">
-                      <li>Code Battles</li>
-                      <li>Submit Assignments</li>
-                      <li>All in one place</li>
-                    </ul>
-                  </center>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <div class="row">
-                <div class="col-sm-7">
-                  <img class="img-responsive" src="{{URL::asset('public/assets/img/template-easy-code.png')}}" alt="">
-                </div>
-                <div class="col-sm-5">
-                  <center>
-                    <h1>
-                      CODE <br>
-                      COMPETE <br>
-                      COLLABORATE
-                    </h1>
-                    <ul class="list-style-none">
-                      <li>Code Battles</li>
-                      <li>Submit Assignments</li>
-                      <li>All in one place</li>
-                    </ul>
-                  </center>
-                </div>
-              </div>
-            </div>
           </div>
         <!-- /.project owl-slider -->
         </div>
@@ -106,18 +64,7 @@ endif;
       <div class="container">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-xs-6 col-sm-3">
-              <div class="home box-simple">
-                <div class="border">
-                  <div class="icon">
-                    <i class="fa fa-globe fa-2x"></i>
-                  </div>
-                </div>
-                <h3>Research</h3>
-                <p>Fifth abundantly made Give sixth hath. Cattle creature i be don't them behold green moved fowl Moved life us beast good yielding. Have bring.</p>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-3">
+            <div class="col-xs-6 col-sm-4">
               <div class="home box-simple">
                 <div class="border">
                   <div class="icon">
@@ -125,21 +72,10 @@ endif;
                   </div>
                 </div>
                   <h3>Code</h3>
-                    <p>Fifth abundantly made Give sixth hath. Cattle creature i be don't them behold green moved fowl Moved life us beast good yielding. Have bring.</p>
+                    <p>Solve the problem by coding in your favourite programming language</p>
               </div>
             </div>
-            <div class="col-xs-6 col-sm-3">
-              <div class="home box-simple">
-                <div class="border">
-                  <div class="icon">
-                    <i class="fa fa-comments-o fa-2x"></i>
-                  </div>
-                </div>
-                <h3>Coordinate</h3>
-                <p>Fifth abundantly made Give sixth hath. Cattle creature i be don't them behold green moved fowl Moved life us beast good yielding. Have bring.</p>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-3">
+            <div class="col-xs-6 col-sm-4">
               <div class="home box-simple">
                 <div class="border">
                   <div class="icon">
@@ -147,7 +83,18 @@ endif;
                   </div>
                 </div>
                 <h3>Test</h3>
-                <p>Fifth abundantly made Give sixth hath. Cattle creature i be don't them behold green moved fowl Moved life us beast good yielding. Have bring.</p>
+                <p>Our system will test your code with verious test case with all the possiblities.</p>
+              </div>
+            </div>
+            <div class="col-xs-6 col-sm-4">
+              <div class="home box-simple">
+                <div class="border">
+                  <div class="icon">
+                    <i class="fa fa-globe fa-2x"></i>
+                  </div>
+                </div>
+                <h3>Compet</h3>
+                <p>Try to optimize your code with new algorithms and compete with your friends.</p>
               </div>
             </div>
           </div>
@@ -155,226 +102,6 @@ endif;
       </div>
     </section>
 
-    <section class="bar background-white no-mb">
-      <div class="container">
-
-          <div class="col-md-12  text-center">
-              <div class=" text-center">
-                        <h3>Let them know why youre the best!</h3>
-                    </div>
-
-                    <p class="lead">Share it. Enjoy It. <span class="accent"> Start Here!</span>
-                    </p>
-
-
-              <div class="row">
-                  <div class="col-md-3 col-sm-6">
-                      <div class="box-image-text blog">
-                          <div class="top">
-                              <div class="image">
-                                        <img src="{{URL::asset('public/assets/img/portfolio-4.jpg')}}" alt="" class="img-responsive">
-                                    </div>
-                              <div class="bg"></div>
-                              <div class="text">
-                                        <p class="buttons">
-                                            <a href="blog-post.html" class="btn btn-template-transparent-primary"><i class="fa fa-plus-circle"></i></a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.box-image-text -->
-
-                        </div>
-                  <div class="col-md-3 col-sm-6">
-                      <div class="box-image-text blog">
-                          <div class="top">
-                              <div class="image">
-                                        <img src="{{URL::asset('public/assets/img/portfolio-4.jpg')}}" alt="" class="img-responsive">
-                                    </div>
-                              <div class="bg"></div>
-                              <div class="text">
-                                        <p class="buttons">
-                                            <a href="blog-post.html" class="btn btn-template-transparent-primary"><i class="fa fa-plus-circle"></i></a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.box-image-text -->
-
-                        </div>
-                  <div class="col-md-3 col-sm-6">
-                      <div class="box-image-text blog">
-                          <div class="top">
-                              <div class="image">
-                                        <img src="{{URL::asset('public/assets/img/portfolio-4.jpg')}}" alt="" class="img-responsive">
-                                    </div>
-                              <div class="bg"></div>
-                              <div class="text">
-                                        <p class="buttons">
-                                            <a href="blog-post.html" class="btn btn-template-transparent-primary"><i class="fa fa-plus-circle"></i></a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.box-image-text -->
-
-                        </div>
-                  <div class="col-md-3 col-sm-6">
-                      <div class="box-image-text blog">
-                          <div class="top">
-                              <div class="image">
-                                        <img src="{{URL::asset('public/assets/img/portfolio-4.jpg')}}" alt="" class="img-responsive">
-                                    </div>
-                              <div class="bg"></div>
-                              <div class="text">
-                                        <p class="buttons">
-                                            <a href="blog-post.html" class="btn btn-template-transparent-primary"><i class="fa fa-plus-circle"></i></a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.box-image-text -->
-
-                        </div>
-
-
-                    </div>
-                    <!-- /.row -->
-
-                    <!-- *** BLOG HOMEPAGE END *** -->
-
-                </div>
-
-            </div>
-            <!-- /.container -->
-        </section>
-        <!-- /.bar -->
-
-          <section class="no-mb">
-      <div class='container-fluid team-wrapper'>
-        <div class="row">
-            <div class="col-xs-6 col-sm-3">
-                <div class="team-member" data-animate="fadeInDown">
-                    <div class="image">
-                              <a href="team-member.html">
-                                  <img src="{{URL::asset('public/assets/img/person.jpg')}}" alt="" class="img-responsive img-circle">
-                              </a>
-                          </div>
-                          <h3><a href="team-member.html">Team Member</a></h3>
-                          <p class="role">Founder</p>
-                      </div>
-                      <!-- /.team-member -->
-                  </div>
-            <div class="col-xs-6 col-sm-3" data-animate="fadeInDown">
-                <div class="team-member">
-                    <div class="image">
-                              <a href="team-member.html">
-                                  <img src="{{URL::asset('public/assets/img/person.jpg')}}" alt="" class="img-responsive img-circle">
-                              </a>
-                          </div>
-                          <h3><a href="team-member.html">Team Member</a></h3>
-                          <p class="role">CTO</p>
-                      </div>
-                      <!-- /.team-member -->
-                  </div>
-            <div class="col-xs-6 col-sm-3" data-animate="fadeInDown">
-                <div class="team-member">
-                    <div class="image">
-                              <a href="team-member.html">
-                                  <img src="{{URL::asset('public/assets/img/person.jpg')}}" alt="" class="img-responsive img-circle">
-                              </a>
-                          </div>
-                          <h3><a href="team-member.html">Team Member</a></h3>
-                          <p class="role">Team Leader</p>
-                      </div>
-                      <!-- /.team-member -->
-                  </div>
-            <div class="col-xs-6 col-sm-3" data-animate="fadeInDown">
-                <div class="team-member">
-                    <div class="image">
-                              <a href="team-member.html">
-                                  <img src="{{URL::asset('public/assets/img/person.jpg')}}" alt="" class="img-responsive img-circle">
-                              </a>
-                          </div>
-                          <h3><a href="team-member.html">Team Member</a></h3>
-                          <p class="role">Lead Developer</p>
-                      </div>
-                      <!-- /.team-member -->
-                  </div>
-              </div>
-              <!-- /.row -->
-            </div>
-          </section>
-
-          <section class="home bar no-mb color-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6">
-                  <div class="embed-responsive embed-responsive-16by9">
-                          <iframe src="https://www.youtube.com/embed/rwy50QEpAsA"> </iframe>
-                        </div>
-                      </div>
-                <div class="col-xs-12 col-sm-6">
-                          <h2>What Are You Waiting For?</h2>
-                          <h2>Start It Today!</h2>
-
-                          <p class="lead">
-                            Explore the best in you, coz the time has arrivedto lift up yourselfat the epitome of ambitions. <br>
-                            Happy Coding! <br><br>
-                            <button type="button" class="btn btn-template-transparent-primary">Find Out More</button>
-                          </p>
-
-                      </div>
-                  </div>
-              </div>
-          </section>
-
-          <section class="no-mb color-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-4">
-                        <h2>EXTRAS</h2>
-
-                        <p class="lead">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta ex accusamus qui ipsa nisi eligendi aliquam omnis voluptatem veniam vero deserunt amet, incidunt harum debitis ullam ratione necessitatibus dignissimos minus.
-                        </p>
-                      </div>
-                <div class="col-sm-8">
-
-                          <h2>Events</h2>
-
-                    <div class="photostream">
-                      <div class="row">
-                        <div class="col-xs-3 col-sm-3">
-                                  <a href="#">
-                                    <img src="{{URL::asset('public/assets/img/detailsquare.jpg')}}" class="img-responsive" alt="#">
-
-                                  </a>
-                              </div>
-                        <div  class="col-xs-3 col-sm-3">
-                                  <a href="#">
-                                      <img src="{{URL::asset('public/assets/img/detailsquare.jpg')}}" class="img-responsive" alt="#">
-                                  </a>
-                              </div>
-                        <div  class="col-xs-3 col-sm-3">
-                                  <a href="#">
-                                      <img src="{{URL::asset('public/assets/img/detailsquare.jpg')}}" class="img-responsive" alt="#">
-                                  </a>
-                              </div>
-                        <div class="col-xs-3 col-sm-3">
-                                  <a href="#">
-                                      <img src="{{URL::asset('public/assets/img/detailsquare.jpg')}}" class="img-responsive" alt="#">
-                                  </a>
-                              </div>
-
-                            </div>
-                          </div>
-
-                      </div>
-                      <!-- /.col-md-3 -->
-                  </div>
-              </div>
-          </section>
   @endsection
   @section('footer')
     @include('master.footer')
--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -6,64 +6,19 @@ _________________________________________________________ -->
 
                 <div class="col-sm-4">
 
-                    <h4>Links</h4>
-
-                    <div class="blog-entries">
-                        <div class="item same-height-row clearfix">
-                            <div class="image same-height-always">
-                                <a href="#">
-                                    <img class="img-responsive" src = "{{ URL::asset('public/assets/img/detailsquare.jpg')}}" alt="">
-                                </a>
-                            </div>
-                            <div class="name same-height-always">
-                                <h5><a href="#">link</a></h5>
-                            </div>
-                        </div>
-
-                        <div class="item same-height-row clearfix">
-                            <div class="image same-height-always">
-                                <a href="#">
-                                    <img class="img-responsive" src = "{{ URL::asset('public/assets/img/detailsquare.jpg')}}" alt="">
-                                </a>
-                            </div>
-                            <div class="name same-height-always">
-                                <h5><a href="#">new link</a></h5>
-                            </div>
-                        </div>
-
-                        <div class="item same-height-row clearfix">
-                            <div class="image same-height-always">
-                                <a href="#">
-                                    <img class="img-responsive" src = "{{ URL::asset('public/assets/img/detailsquare.jpg')}}" alt="">
-                                </a>
-                            </div>
-                            <div class="name same-height-always">
-                                <h5><a href="#">another link </a></h5>
-                            </div>
-                        </div>
-                    </div>
-
-                    <hr class="hidden-md hidden-lg hidden-sm">
-
-                </div>
-                <!-- /.col-md-3 -->
-
-                <div class="col-sm-4">
-
                     <h4>Contact Us</h4>
 
-                    <p><strong>JSS Academy Of Technical Education</strong>
-                        <br><br>
-                        <strong>Address: </strong>
-                        <br> C-20/1
-                        <br>Sector 62, Noida
-                        <br>Uttar Pradesh
-                        <br>201301
-                        <br>
-                        India
-                        <br><br>
-                        <strong>Phone:</strong> 0120 240 0104
-                    </p>
+                    <p>
+                            <img src="http://jssaten.ac.in/img/favicon.ico" rel="shortcut icon" type="image/icon"> JSS Academy Of Technical Education
+                            <br>
+                            <i style="color:#fff" class="fa fa-home"></i>&nbsp;C-20/1, Sector-62, NOIDA (U.P)-201301
+                            <br>
+                            <i style="color:#fff" class="fa fa-phone"></i>&nbsp; Phone : 0120-2400104, 2400115
+                            <br>
+                            <i style="color:#fff" class="fa fa-fax"></i>&nbsp;Fax : 0120-2400097
+                            <br>
+                            <i style="color:#fff" class="fa fa-envelope"></i>&nbsp;principal@jssaten.ac.in
+                        </p>
 
                     <hr class="hidden-md hidden-lg hidden-sm">
 
@@ -72,21 +27,35 @@ _________________________________________________________ -->
 
                 <div class="col-sm-4">
 
-                  <h4>Social Media</h4>
-                  <br>
-                  <p>Connect with us here.</p>
-                  <br>
-                  <div class="social">
-                      <a href="#" class="external youtube" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-youtube"></i></a>
-                      <a href="#" class="external facebook" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-facebook"></i></a>
-                      <a href="#" class="external gplus" data-animate-hover="pulse" style="opacity: 1;"><i class="fa fa-google-plus"></i></a>
-                      <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
-                      <a href="#" class="external instagram" data-animate-hover="pulse"><i class="fa fa-instagram"></i></a>
-                  </div>
-                  <br><br>
-                  <p> Copyright &copy; 2015 <strong><a href="http://www.hackncs.com">Nibble Computer Society</a></strong></p>
+                    <h4>Quick Links</h4>
+
+                    <ul>
+                        <li><u><a href="http://jssaten.ac.in/" target="_blank">JSSATEN</a></u></li>
+                        <li><u><a href="https://aktu.ac.in/" target="_blank">AKTU</a></u></li>
+                        <li><u><a href="http://210.212.85.155/" target="_blank">InfoConnect</a></u></li>
+                    </ul>
+
+                    <hr class="hidden-md hidden-lg hidden-sm">
+
                 </div>
                 <!-- /.col-md-3 -->
+
+                <div class="col-sm-4">
+
+                  <h4>Important Link</h4>
+                    <ul>
+                        <li><u><a href="https://www.geeksforgeeks.org/" target="_blank">GeeksForGeeks</a></u></li>
+                        <li><u><a href="https://www.codechef.com/" target="_blank">CodeChef</a></u></li>
+                        <li><u><a href="https://www.hackerrank.com/" target="_blank">HackerRank</a></u></li>
+                        <li><u><a href="https://www.hackerearth.com/" target="_blank">HackerEarth</a></u></li>
+                    </ul>
+                  
+                </div>
+                <!-- /.col-md-3 -->
+            </div>
+            <!-- /.container -->
+            <div class="container">
+                <center> Copyright &copy; {{ date("Y") }} <strong><a target="_blank" href="http://www.hackncs.com">Nibble Computer Society</a></strong></center>
             </div>
             <!-- /.container -->
         </footer>
